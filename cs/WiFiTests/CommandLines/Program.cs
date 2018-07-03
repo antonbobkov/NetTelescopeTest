39a7ebbebafad4e82226d0173bcbe0a7af3d1af5
AntonLaptop <texfan@gmail.com>
2018-07-02 19:42:20 -0700

    cmd

---
+++ cs/WiFiTests/CommandLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLines
+{
+    class Program
+    {
+        static void RunCommand(string cmd) {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + cmd;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args[0] == "hon")
+                RunCommand("netsh wlan start hostednetwork");
+            else if (args[0] == "hoff")
+                RunCommand("netsh wlan stop hostednetwork");
+            else if (args[0] == "won")
+                RunCommand("netsh interface set interface \"Wi-Fi\" enabled");
+            else if (args[0] == "woff")
+                RunCommand("netsh interface set interface \"Wi-Fi\" disabled");
+            else
+                System.Console.WriteLine("Unknown command " + args[0] + "\n");
+        }
+    }
+}
---
+++ cs/WiFiTests/RadioOnOff/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioOnOff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var access = await WiFiAdapter.RequestAccessAsync();
+            if (access == WiFiAccessStatus.Allowed)
+            {
+                var radios = await Radio.GetRadiosAsync();
+                foreach (var radio in radios)
+                {
+                    if (radio.Kind == RadioKind.WiFi)
+                    {
+                        await radio.SetStateAsync(RadioState.Off);
+                    }
+                }
+            }
+        }
+    }
+}
